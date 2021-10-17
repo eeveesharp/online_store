a295c16c8bc7dfd76267cadf77e45827b8b16604
maximShamigovKrutoiChel <hui1337228.nagibator>
2021-10-17 17:24:12 +0300

    added IFile

---
+++ online_store/online-store/ConsoleApp2/IFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Online_Shop
+{
+    public interface IFile
+    {
+        public void Write(IEnumerable<object> items,
+            string fileName);
+
+        public void Read(string fileName);
+
+        public void ReadProduct(string fileName);
+
+        public void ReadHistoryBuy(string fileName);
+
+        public string ReadTextFromFile(string filename);
+
+    }
+}
